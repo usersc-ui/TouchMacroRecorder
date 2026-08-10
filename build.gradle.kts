569960d7fdd9987de17b4752351a72288f66c8a3
usersc-ui <clansofclansalt01@gmail.com>
2026-08-10 22:02:42 +0200

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,15 @@
 plugins {
-    id("com.android.application") version "8.6.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+android {
+    namespace = "com.example.touchmacro"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.example.touchmacro"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
 }
