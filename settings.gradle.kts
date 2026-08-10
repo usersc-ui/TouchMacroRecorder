6ab5da5a5cc892a55b4d19a0c89700beca18b027
usersc-ui <clansofclansalt01@gmail.com>
2026-08-10 22:27:52 +0200

    Update settings.gradle.kts for plugin and dependency management
    Refactor Gradle settings to use plugin management and dependency resolution management.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,19 @@
-plugins {
-    id("com.android.application") version "8.6.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TouchMacroRecorder"
+
+include(":app")
