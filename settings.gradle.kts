1ecc2b32f8d12cc4421046cc8d45a07623f334b6
usersc-ui <clansofclansalt01@gmail.com>
2026-08-10 22:25:02 +0200

    Refactor settings.gradle.kts to simplify configuration
    Removed plugin and dependency resolution management sections.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,4 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
+plugins {
+    id("com.android.application") version "8.6.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "TouchMacroRecorder"
-include(":app")
